54f52ef76ab5a0789f89e7da4cb891eb77f0158f
SirSaar <saar643@gmail.com>
2017-12-25 11:58:15 +0000

    vagrant server

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var app = express();
 
 var mongoose = require('mongoose');
 mongoose.Promise = require('bluebird');
-mongoose.connect('mongodb://localhost/mean-angular5', { useMongoClient: true, promiseLibrary: require('bluebird') })
+mongoose.connect('mongodb://admin:admin@ds113136.mlab.com:13136/comarket', { useMongoClient: true, promiseLibrary: require('bluebird') })
   .then(() =>  console.log('connection succesful'))
   .catch((err) => console.error(err));
 
