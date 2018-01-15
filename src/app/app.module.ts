4644b39f68b629d8d010ceebe5ff85f1bddafa9e
SirSaar <saar643@gmail.com>
2018-01-15 13:48:48 +0200

    before auth

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,49 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from '@angular/common/http';
 import {APP_BASE_HREF} from '@angular/common';
 
-import { AppComponent } from './app.component';
-import { MyStoreComponent } from './my-store/my-store.component';
-
-//saar
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
-import { RouterModule, Routes } from '@angular/router';
 import { SplitPipe } from './split.pipe';
-import { ItemCreateComponent } from './item-create/item-create.component';
+import { ItemService } from './item.service';
+//import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+
+import { MarketComponent } from './market/market.component';
+import { MyStoreComponent } from './my-store/my-store.component';
 import { PageNotFoundComponentComponent } from './page-not-found-component/page-not-found-component.component';
 import { ItemEditComponent } from './item-edit/item-edit.component';
-import { ItemService } from './item.service';
-import { MarketComponent } from './market/market.component';
-
-const appRoutes: Routes = [
-  {
-    path: 'market',
-    component: MarketComponent,
-    data: { title: 'coMarket' }
-  },
-  {
-    path: 'my-store',
-    component: MyStoreComponent,
-    data: { title: 'My Store' }
-  },
-  {
-    path: 'item-create',
-    component: ItemCreateComponent,
-    data: { title: 'Add New Item' }
-  },
-  {
-    path: 'item-edit/:id',
-    component: ItemEditComponent,
-    data: { title: 'Edit Item' }
-  },
-  { path: '',
-    redirectTo: '/my-store',
-    pathMatch: 'full'
-  },
-  { path: '**', component: PageNotFoundComponentComponent }
-];
+import { ItemCreateComponent } from './item-create/item-create.component';
 
 @NgModule({
   declarations: [
@@ -59,10 +31,7 @@ const appRoutes: Routes = [
     BrowserModule,
     FormsModule,
     HttpClientModule,
-    RouterModule.forRoot(
-      appRoutes,
-      { enableTracing: true } // <-- debugging purposes only
-    )
+    AppRoutingModule
   ],
   providers: [{provide: APP_BASE_HREF, useValue : '/'}, ItemService ],
   bootstrap: [AppComponent]
