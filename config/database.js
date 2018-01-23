90a8f07117dd26209168e9d4d74494241ba6a158
SirSaar <saar643@gmail.com>
2018-01-23 22:01:49 +0200

    refresh config files .gitignore

--- config/database.js
+++
@@ -1,8 +0,0 @@
-module.exports={
-    "mongo":{
-        "development":{
-            "connectionString":"mongodb://admin:admin@ds113136.mlab.com:13136/comarket"
-        },
-        "production":{}
-    }
-}
